762135eaccf8ace48ab5234a3f124d6dc0e216b0
RohanBhattB <shirish.rohanbirudaraju@gmail.com>
2024-10-04 23:05:26 +0530

    plan

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class main{
+    
+    //method to check weather if the password is Strong
+        //if password has 7 characters
+        //if password has atleast one special character
+        //if password has atleast one number in it
+        //check if password in the default password file
+}
+//main file 
